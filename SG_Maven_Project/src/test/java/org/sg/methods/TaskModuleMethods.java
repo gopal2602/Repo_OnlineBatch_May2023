66b153c0fbdac34e2a255541e32d4eb44eb45e52
sgTesting <gopal.gudi@gmail.com>
2023-05-29 08:44:22 +0530

    aded taskModule method & created new class file

--- SG_Maven_Project/src/test/java/org/sg/methods/TaskModuleMethods.java
+++ SG_Maven_Project/src/test/java/org/sg/methods/TaskModuleMethods.java
@@ -1,7 +1,35 @@
 package org.sg.methods;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.sg.driver.DriverScript;
 
 public class TaskModuleMethods extends DriverScript{
-
+	/******************************************
+	 * Method Name		: getPropData
+	 * 
+	 * 
+	 * 
+	 ******************************************/
+	public String getPropData(String keyName) {
+		FileInputStream fin = null;
+		Properties prop = null;
+		try {
+			fin = new FileInputStream(System.getProperty("user.dir") + "\\Configuration\\configuration.properties");
+			prop = new Properties();
+			prop.load(fin);
+			return prop.getProperty(keyName);
+		}catch(Exception e) {
+			System.out.println("Exception in the 'getPropData()' method " + e);
+			return null;
+		}finally
+		{
+			try {
+				fin.close();
+				fin = null;
+				prop = null;
+			}catch(Exception e) {}
+		}
+	}
 }
